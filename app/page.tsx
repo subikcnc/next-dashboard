f340faf3968e463d4519c4a9aeb9e194657d10cd
cnc <subik@curvesncolors.ch>
2024-07-08 15:04:36 +0545

    streaming for revenueChart and latestInvoices components

--- app/page.tsx
+++ app/page.tsx
@@ -4,50 +4,114 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Badge } from '@/components/ui/badge';
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <AcmeLogo />
-      </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-        <div
-  className={styles.shape}
-/>
-          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
-          </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
+    <>
+      <main className="flex min-h-screen flex-col p-6">
+        <div className=" h-20 flex shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+          <AcmeLogo />
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
-          <Image
-        src="/hero-desktop.png"
-        width={1000}
-        height={760}
-        className="hidden md:block"
-        alt="Screenshots of the dashboard project showing desktop version"
-      />
-          <Image
-        src="/hero-mobile.png"
-        width={560}
-        height={620}
-        className="hidden max-md:block"
-        alt="Screenshots of the dashboard project showing desktop version"
-      />
+        <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+          <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+            <div className={styles.shape} />
+            <p
+              className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+            >
+              <strong>Welcome to Acme.</strong> This is the example for the{' '}
+              <a href="https://nextjs.org/learn/" className="text-blue-500">
+                Next.js Learn Course
+              </a>
+              , brought to you by Vercel.
+            </p>
+            <Link
+              href="/login"
+              className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            >
+              <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            </Link>
+            <Button variant="pizza" size="sm">Log in</Button>
+            <Badge >Pizza badge</Badge>
+            <Input type="email" placeholder="Email Address" />
+          </div>
+          <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+            {/* Add Hero Images Here */}
+            <Image
+              src="/hero-desktop.png"
+              width={1000}
+              height={760}
+              className="hidden md:block"
+              alt="Screenshots of the dashboard project showing desktop version"
+            />
+            <Image
+              src="/hero-mobile.png"
+              width={560}
+              height={620}
+              className="hidden max-md:block"
+              alt="Screenshots of the dashboard project showing desktop version"
+            />
+          </div>
+          {/*  */}
+
+          {/*  */}
+        </div>
+      </main>
+      {/*  */}
+      <div className="flex min-h-full flex-col">
+        <header className="shrink-0 bg-gray-900">
+          <div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
+            <img
+              className="h-8 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+              alt="Your Company"
+            />
+            <div className="flex items-center gap-x-8">
+              <button
+                type="button"
+                className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-300"
+              >
+                <span className="sr-only">View notifications</span>
+                <svg
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+                  />
+                </svg>
+              </button>
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">Your profile</span>
+                <img
+                  className="h-8 w-8 rounded-full bg-gray-800"
+                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+              </a>
+            </div>
+          </div>
+        </header>
+
+        <div className="mx-auto w-full max-w-7xl grow lg:flex xl:px-2">
+          <div className="flex-1 xl:flex">
+            <div className="border-b border-gray-200 px-4 py-6 sm:px-6 lg:pl-8 xl:w-64 xl:shrink-0 xl:border-b-0 xl:border-r xl:pl-6"></div>
+
+            <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6"></div>
+          </div>
+
+          <div className="shrink-0 border-t border-gray-200 px-4 py-6 sm:px-6 lg:w-96 lg:border-l lg:border-t-0 lg:pr-8 xl:pr-6"></div>
         </div>
       </div>
-    </main>
+      {/*  */}
+    </>
   );
 }
